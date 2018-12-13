74d7b1eecff69aaaf4577fca070e14c73802e397
Feihong Hsu <feihong.hsu@gmail.com>
2018-12-13 07:29:30 -0600

    Add csv file

--- generate.js
+++ generate.js
@@ -1,11 +1,21 @@
 require('isomorphic-fetch')
 const parse = require('csv-parse/lib/sync')
+const fs = require('fs')
 
+const csvFile = 'webcomics.csv'
 const url = 'https://docs.google.com/spreadsheets/d/1VFy6jdPbRjZiQJ2a0fn9eFnAcrQh5ebSh21tTihKeKA/export?format=csv'
 
 fetch(url)
 .then(response => response.text())
 .then(text => {
+  let origText = fs.existsSync(csvFile) ?
+    fs.readFileSync(csvFile, 'utf8') : ''
+  if (text === origText) {
+    return
+  }
+  console.log('Detected changes, updating...');
+  fs.writeFileSync(csvFile, text, 'utf8')
+
   let lines = text.split('\n')
   lines[0] = lines[0].toLowerCase().replace(/ /g, '_')
   let rows = parse(lines.join('\n'), {columns: true})
