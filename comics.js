c24f756228b8eff603d7cdd1e16c8621dd27b71a
Feihong Hsu <feihong.hsu@gmail.com>
2018-12-22 22:28:33 -0600

    Download bilingual comics csv as well

--- comics.js
+++ comics.js
@@ -2,29 +2,36 @@ require('isomorphic-fetch')
 const parse = require('csv-parse/lib/sync')
 const fs = require('fs')
 
-const csvFile = 'webcomics.csv'
-const url = 'https://docs.google.com/spreadsheets/d/1VFy6jdPbRjZiQJ2a0fn9eFnAcrQh5ebSh21tTihKeKA/export?format=csv'
+const awesomeFile = 'awesome.csv'
+const awesomeUrl = 'https://docs.google.com/spreadsheets/d/1VFy6jdPbRjZiQJ2a0fn9eFnAcrQh5ebSh21tTihKeKA/export?format=csv'
+
+const bilingualFile = 'bilingual.csv'
+const bilingualUrl = 'https://docs.google.com/spreadsheets/u/1/d/1VFy6jdPbRjZiQJ2a0fn9eFnAcrQh5ebSh21tTihKeKA/export?format=csv&id=1VFy6jdPbRjZiQJ2a0fn9eFnAcrQh5ebSh21tTihKeKA&gid=1749138200'
 
 function main() {
+  fetchToFile(awesomeUrl, awesomeFile)
+  fetchToFile(bilingualUrl, bilingualFile)
+}
+
+function fetchToFile(url, filename) {
   fetch(url)
     .then(response => response.text())
     .then(text => {
-      fetchComics(text)
-      console.log(getComics())
+      fetchComics(text, filename)
     })
 }
 
-function fetchComics(text) {
-  let origText = fs.existsSync(csvFile) ?
-    fs.readFileSync(csvFile, 'utf8') : ''
+function fetchComics(text, filename) {
+  let origText = fs.existsSync(filename) ?
+    fs.readFileSync(filename, 'utf8') : ''
   if (text === origText) {
     return
   }
-  console.log('Detected changes, updating...');
-  fs.writeFileSync(csvFile, text, 'utf8')
+  console.log(`Detected changes, updating ${filename}`);
+  fs.writeFileSync(filename, text, 'utf8')
 }
 
-function getComics() {
+function getComics(csvFile) {
   let text = fs.readFileSync(csvFile, 'utf8')
   let lines = text.split('\n')
   lines[0] = lines[0].toLowerCase().replace(/ /g, '_')
@@ -44,6 +51,14 @@ function getComics() {
   genres.sort()
 
   return { comics, lastUpdated, genres }
+}
+
+function getAwesomeComics() {
+  return getComics(awesomeFile)
+}
+
+function getBilingualComics() {
+  return getComics(bilingualFile)
 }
 
 module.exports = { getComics }
