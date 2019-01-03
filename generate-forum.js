1b23105d2bf711f5f85f1596d81a4ab3320a56a7
Feihong Hsu <feihong.hsu@gmail.com>
2019-01-03 12:46:43 -0600

    Generate forum markup for awesome comics

--- generate-forum.js
+++ generate-forum.js
@@ -1,34 +1,37 @@
 // Generate HTML to copy and paste into chinese-forums.com
 const fs = require('fs')
-const { getBilingualComics } = require('./comics')
+const { getBilingualComics, getAwesomeComics } = require('./comics')
 const nunjucks = require('nunjucks')
 
 function main() {
-  let comics = getBilingualComics().comics
-
-
   nunjucks.configure('templates')
+
+  generateHtml(getBilingualComics().comics, 'bilingual-forum.html')
+  generateHtml(getAwesomeComics().comics, 'awesome-forum.html')
+}
+
+function generateHtml(comics, filename) {
   html = nunjucks.render('forum.html', {comics: addFields(comics)})
-  fs.writeFileSync('forum.html', html, 'utf8')
-  console.log('Generated forum.html')
+  fs.writeFileSync(filename, html, 'utf8')
+  console.log(`Generated ${filename}`)
 }
 
 function addFields(comics) {
   comics.map(comic => {
     comic.titles = [
+      comic.title,
       comic.english_title,
       comic.simplified_title,
       comic.traditional_title,
     ]
-    .filter(s => s.trim() !== '')
+    .filter(s => s !== undefined && s.trim() !== '')
 
     comic.links = [
       ['english', comic.english_link],
       ['simplified', comic.simplified_link],
       ['traditional', comic.traditional_link],
     ]
-    .filter(pair => pair[1].trim() !== '')
-    // comic.links = new Map(comic.links)
+    .filter(pair => pair[1] !== undefined && pair[1].trim() !== '')
 
     return comic
   })
